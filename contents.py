485cf541c2f59480d00f1e3cbb9f4e53b31c35ca
Ekram <ekruiwc2020@gmail.com>
2023-03-27 16:39:10 +0300

    modified content.py file with snippet_2.py file

--- contents.py
+++ contents.py
@@ -1,1 +1,23 @@
 # add your contents here
+def is_valid_parenthesis(s):
+    dc = {
+        '(': ')',
+          "{": "}",
+         '[': ']'
+
+    }
+    stack = []
+    for i in s:
+        if i in dc.keys(): stack.append(i)
+        else:
+            if len(stack) == 0:
+                return False
+            
+
+            op = stack.pop()
+            if dc[op] != i:
+                return False
+    if len(stack) != 0:
+        return False
+    
+    return True
